731478d7fae57ce13b4cf6b9540024f04952010e
jodis <jodannysaliendres@gmail.com>
2024-04-02 12:47:26 -0500

    listo para deploy

--- proyecto_pi/api/src/db.js
+++ proyecto_pi/api/src/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false,
   native: false,
 });
