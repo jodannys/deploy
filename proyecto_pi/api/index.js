77e79f34ffb499b4328c02b0144b601d2847b931
jodis <jodannysaliendres@gmail.com>
2024-04-02 14:57:21 -0500

    entorno

--- proyecto_pi/api/index.js
+++ proyecto_pi/api/index.js
@@ -1,12 +1,22 @@
+// const server = require('./src/app.js');
+// const { conn } = require('./src/db.js');
+
+
+// // Syncing all the models at once.
+// conn.sync({ force:true}).then(() => {
+//   const PORT=process.env.PORT
+//   server.listen(3001, () => {
+//     // console.log('%s listening at 3001'); 
+//     console.log("Alicacion corriendo en el puerto "+ PORT ); 
+//   });
+// });
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-
 // Syncing all the models at once.
-conn.sync({ force:true}).then(() => {
-  const PORT=process.env.PORT
-  server.listen(3001, () => {
-    // console.log('%s listening at 3001'); 
-    console.log("Alicacion corriendo en el puerto "+ PORT ); 
+conn.sync({ force: true }).then(() => {
+  const PORT = process.env.PORT || 3001; 
+  server.listen(PORT, () => {
+    console.log("Aplicación corriendo en el puerto " + PORT);
   });
 });
